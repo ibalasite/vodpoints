03fe68ef9ec2879f543c829a8c68db5376fdafcc
Kevin Tan <kevintan1983@hotmail.com>
2015-12-01 01:08:05 +0800

    頻道商管理新增與刪除功能，mysql套件安裝，使用元件Connector.NET 6.9 的 6.9.7 版本

---
+++ source/vodpoints/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vodpoints.edmx;
+
+namespace vodpoints.Controllers
+{
+    public class BaseController : Controller
+    {
+        public vodpointsdbEntities vodpointsdb = new vodpointsdbEntities();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.vodpointsdb.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+	}
+}
---
+++ source/vodpoints/Controllers/SupplierController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+using vodpoints.edmx;
+
+namespace vodpoints.Controllers
+{
+    public class SupplierController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var suppliers = this.vodpointsdb.suppliers.ToList();
+            return View(suppliers);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                this.vodpointsdb.suppliers.Add(supplier);
+                this.vodpointsdb.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                this.vodpointsdb.Entry(supplier).State = EntityState.Modified;
+                this.vodpointsdb.SaveChanges();
+            }
+
+            return View(supplier);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var supplierToDelte = this.vodpointsdb.suppliers.Find(id);
+            this.vodpointsdb.suppliers.Remove(supplierToDelte);
+            this.vodpointsdb.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
